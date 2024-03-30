eb9365dc1d3908316b6ef0ac1f298003d983a4d0
BuildTools <unconfigured@null.spigotmc.org>
2024-03-30 16:25:17 -0500

    cleanup

--- scene/Lever.gd
+++ scene/Lever.gd
@@ -1,28 +1,30 @@
 extends Node2D
 
 @export var base_speed: float = 0.2
-@export var counter_clockwise: bool = true 
+@export var counter_clockwise: bool = true
 
-@onready var body = %Body
+@onready var body: StaticBody2D = %Body
 
-var speed: float = 0
+var speed: float    = 0
 var direction: int
 var activated: bool = false
 
+
 func _ready():
-	if counter_clockwise:
-		direction = -1
-	else:
-		direction = 1
+    if counter_clockwise:
+        direction = -1
+    else:
+        direction = 1
+
 
 func _process(delta):
-	if activated:	
-		body.rotate(speed * direction)
+    if activated:
+        body.rotate(speed * direction)
 
 
 func _on_speed_slider_value_changed(value):
-	if value <= 0:
-		activated = false
-	else:
-		activated = true
-	speed = base_speed * value
+    if value <= 0:
+        activated = false
+    else:
+        activated = true
+    speed = base_speed * value
