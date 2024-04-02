8eed332fa202656b25a26c8d2465bf3942cf5c13
BuildTools <unconfigured@null.spigotmc.org>
2024-04-01 19:19:36 -0500

    naming conventions for scripts

--- scene/Playfield.gd
+++ scene/Playfield.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 @onready var display_panel = %DisplayPanel
-@onready var label_1 = %Label1
-@onready var label_2 = %Label2
-@onready var label_3 = %Label3
+@onready var label_1: Label = %Label1
+@onready var label_2: Label = %Label2
+@onready var label_3: Label = %Label3
 @onready var crit = %crit
 
 @export var health: int = 1000
